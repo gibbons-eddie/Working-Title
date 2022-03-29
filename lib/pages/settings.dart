c8fb0451ef16ef590692e9e2ad864ed5ffa3b3df
YuchenXiong <xiongyuchen@ufl.edu>
2022-03-28 20:05:28 -0400

    trying to get the api running in setting page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:senior_project/main_layout/custom_page.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+
+void playAudio() {
+  final assetsAudioPlayer = AssetsAudioPlayer();
+  assetsAudioPlayer.open(Audio("assets/audios/song1.mp3"));
+}
 
 var settings = const CustomPage(
   title: 'Settings',
   icon: Icons.settings,
   child: Center(
-    child: Text('Settings'),
+    child: Text('Setting'),
   ),
 );
