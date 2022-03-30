73575e1abab5c279fcdd625d677a59b9937eb328
YuchenXiong <xiongyuchen@ufl.edu>
2022-03-29 20:03:26 -0400

    Updated a text based setting page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,16 +1,44 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:senior_project/main_layout/custom_page.dart';
-import 'package:assets_audio_player/assets_audio_player.dart';
+//import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:english_words/english_words.dart';
 
-void playAudio() {
-  final assetsAudioPlayer = AssetsAudioPlayer();
-  assetsAudioPlayer.open(Audio("assets/audios/song1.mp3"));
-}
-
-var settings = const CustomPage(
+var settings = CustomPage(
   title: 'Settings',
   icon: Icons.settings,
-  child: Center(
-    child: Text('Setting'),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: const [
+      Text(
+        'Study Summary',
+        style: TextStyle(color: Colors.white, fontSize: 21),
+      ),
+      Divider(color: Colors.white),
+      Text(
+        'Overall Performance',
+        style: TextStyle(color: Colors.white, fontSize: 21),
+      ),
+      Divider(color: Colors.white),
+      Text(
+        'Saved Modules',
+        style: TextStyle(color: Colors.white, fontSize: 21),
+      ),
+      Divider(color: Colors.white),
+      Text(
+        'Volume Setting',
+        style: TextStyle(color: Colors.white, fontSize: 21),
+      ),
+      Divider(color: Colors.white),
+      Text(
+        'Privacy Setting',
+        style: TextStyle(color: Colors.white, fontSize: 21),
+      ),
+      Divider(color: Colors.white),
+      Text(
+        'About',
+        style: TextStyle(color: Colors.white, fontSize: 21),
+      ),
+    ],
   ),
 );
