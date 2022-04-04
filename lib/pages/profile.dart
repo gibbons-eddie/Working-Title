d0fff6c4aed19d791ab7a526e52ad66c2ae1c32d
briankoehler <briankoehler@ufl.edu>
2022-04-04 16:20:39 -0400

    Get completed modules from db

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,21 +1,70 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
 import 'package:senior_project/main_layout/custom_page.dart';
 import 'package:senior_project/profile/profile_section.dart';
 import 'package:senior_project/theme.dart';
 import 'package:senior_project/profile/profile_sections.dart'
     as profile_sections;
+import 'package:sqflite/sqflite.dart';
+
+// Use this function to copy the database from the assets folder to the app's
+// database folder or use the existing copy. I'm guessing that if you update
+// the database in the assets folder, it won't copy it again, so you'll have to
+// delete the existing copy somehow.
+Future<Database> _getDatabase() async {
+  // Get the path to the database.
+  var databasesPath = await getDatabasesPath();
+  var path = join(databasesPath, 'senior_project_app.db');
+
+  // Check if the database exists.
+  var exists = await databaseExists(path);
+
+  // Make a copy from the asset folder.
+  if (!exists) {
+    if (kDebugMode) print('Creating new copy from asset.');
+
+    try {
+      await Directory(dirname(path)).create(recursive: true);
+    } catch (_) {}
+
+    // Copy from asset.
+    ByteData data =
+        await rootBundle.load(join('assets/db', 'senior_project_app.db'));
+    List<int> bytes =
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    // Write and flush the bytes written.
+    await File(path).writeAsBytes(bytes, flush: true);
+  } else {
+    if (kDebugMode) print('Opening existing database.');
+  }
+
+  return await openDatabase(path);
+}
 
 // Placeholder method to gather data from SQLite database
-Future collectData() {
-  return Future.delayed(
-    const Duration(seconds: 3),
-    () => {
-      'name': 'Jane Doe',
-      'completed_modules': 3,
-      'quickchats_practiced': 28,
-      'exercises_completed': 43,
-    },
-  );
+Future<Map<String, dynamic>> collectData() async {
+  // return Future.delayed(
+  //   const Duration(seconds: 3),
+  //   () => {
+  //     'name': 'Jane Doe',
+  //     'completed_modules': 3,
+  //     'quickchats_practiced': 28,
+  //     'exercises_completed': 43,
+  //   },
+  // );
+  Database db = await _getDatabase();
+  final completedModules = (await db
+          .rawQuery('SELECT * FROM modules_table WHERE user_completed == 1'))
+      .length;
+  return {
+    'completed_modules': completedModules,
+    'quickchats_practiced': 0,
+    'exercises_completed': 0,
+  };
 }
 
 var profile = CustomPage(
