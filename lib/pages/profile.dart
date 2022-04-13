38f9caa9b715dceaa7be02c557b8565bcb5f13d4
briankoehler <briankoehler@ufl.edu>
2022-04-13 19:00:38 -0400

    Convert profile to stateful widget to re-collect data on each visit

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -49,74 +49,95 @@ Future<Map<String, dynamic>> _collectData() async {
   return result;
 }
 
+class _RealProfile extends StatefulWidget {
+  @override
+  _RealProfileState createState() => _RealProfileState();
+}
+
+class _RealProfileState extends State<_RealProfile> {
+  late Future<Map<String, dynamic>> _data;
+
+  @override
+  void initState() {
+    _data = _collectData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 32),
+      child: SubContainer(
+        child: FutureBuilder<Map<String, dynamic>>(
+          future: _data,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              // Get module progress
+              final List<double> moduleProgress = [
+                snapshot.data['module1'],
+                snapshot.data['module2'],
+                snapshot.data['module3'],
+              ];
+
+              // Get completed phrases
+              final Set<Phrase> completedPhrases =
+                  snapshot.data['completedPhrases'];
+
+              // Calculate completed exercises from completed phrases
+              var completedExercises = 0;
+              for (final p in completedPhrases) {
+                if (p.completed) completedExercises++;
+                if (p.advancedCompleted) completedExercises++;
+              }
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Stats
+                  ProfileSection(
+                    title: 'Statistics',
+                    child: profile_sections.Statistics(
+                      completedModules: moduleProgress
+                          .where((element) => element == 1)
+                          .length,
+                      exercisesCompleted: completedExercises,
+                      moduleProgress: moduleProgress,
+                    ),
+                  ),
+
+                  // Spacing
+                  const SizedBox(height: 36),
+
+                  // Completed modules
+                  ProfileSection(
+                    title: 'Completed Modules',
+                    child: profile_sections.CompletedModules(
+                        moduleProgress: moduleProgress),
+                  ),
+                ],
+              );
+            }
+
+            // Handle error
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+
+            // Handle loading
+            return const Center(
+              child: CircularProgressIndicator(
+                color: CustomColors.lightGreen,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
 var profile = CustomPage(
   title: 'Statistics',
   icon: Icons.portrait,
-  child: Container(
-    margin: const EdgeInsets.only(bottom: 32),
-    child: SubContainer(
-      child: FutureBuilder<Map<String, dynamic>>(
-        future: _collectData(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            // Get module progress
-            final List<double> moduleProgress = [
-              snapshot.data['module1'],
-              snapshot.data['module2'],
-              snapshot.data['module3'],
-            ];
-
-            // Get completed phrases
-            final Set<Phrase> completedPhrases =
-                snapshot.data['completedPhrases'];
-
-            // Calculate completed exercises from completed phrases
-            var completedExercises = 0;
-            for (final p in completedPhrases) {
-              if (p.completed) completedExercises++;
-              if (p.advancedCompleted) completedExercises++;
-            }
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Stats
-                ProfileSection(
-                  title: 'Statistics',
-                  child: profile_sections.Statistics(
-                    completedModules:
-                        moduleProgress.where((element) => element == 1).length,
-                    exercisesCompleted: completedExercises,
-                    moduleProgress: moduleProgress,
-                  ),
-                ),
-
-                // Spacing
-                const SizedBox(height: 36),
-
-                // Completed modules
-                ProfileSection(
-                  title: 'Completed Modules',
-                  child: profile_sections.CompletedModules(
-                      moduleProgress: moduleProgress),
-                ),
-              ],
-            );
-          }
-
-          // Handle error
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
-
-          // Handle loading
-          return const Center(
-            child: CircularProgressIndicator(
-              color: CustomColors.lightGreen,
-            ),
-          );
-        },
-      ),
-    ),
-  ),
+  child: _RealProfile(),
 );
